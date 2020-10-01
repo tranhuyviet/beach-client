e4234015e7f15ebfb47de219f62acf43978950c9
Viet Tran <tranhuyviet@gmail.com>
2020-10-01 14:57:02 +0300

    changed to heroku server

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -3,7 +3,8 @@ import App from './App';
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000',
+    // uri: 'http://localhost:5000',
+    uri: 'https://beach-server.herokuapp.com/',
 });
 
 const client = new ApolloClient({
